eb26b67c747eb393b5281cd4f56090c0f9ca4665
Francisco.I <francisco.insaurralde21@gmail.com>
2024-04-11 13:00:25 -0300

    feature: progressBar component complete and some styles improved

--- src/app/components/progressBar.tsx
+++ src/app/components/progressBar.tsx
@@ -1,15 +1,42 @@
 "use client"
 import React from 'react';
 import { useAppContext } from '../context';
-
+import 'tailwindcss/tailwind.css';
 
 const ProgressBar = () => {
   const { storage } = useAppContext();
-  console.log(storage.progress)
+  let textBar = "Barra de Progreso"
+  // Función para calcular el ancho de la barra de progreso en función del valor de storage.progress
+  const calculateWidth = () => {
+    switch (storage.progress) {
+      case 1:
+        textBar = "Seleccionar servicio"
+        return '20%';
+      case 2:
+        textBar = "Seleccionar horario"
+        return '65%';
+      case 3:
+        textBar = "Confirmar turno"
+        return '85%';
+      default:
+        textBar = "Turnos reservados"
+        return '100%'; // Si el progreso es mayor que 3 o menor que 1, no se muestra la barra de progreso
+    }
+  };
+
+  // Estilo dinámico para la barra de progreso
+  const progressBarStyle = {
+    width: calculateWidth(),
+    background: '#1aae9f',
+    transition: 'width 0.5s ease-in-out',
+  };
+
   return (
-    <div className={"justify-start"}>
-      {/* This contains the progress bar for the ordering process"*/}
-      {storage.progress}
+    <div className=" w-11/12 mt-4 mb-4">
+      <p className="text-sm mb-1 font-bold text-gray-800">{textBar}</p>
+      <div className="bg-gray-200 h-4 rounded overflow-hidden">
+        <div style={progressBarStyle} className="h-full"></div>
+      </div>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col h-screen justify-between`}>
+      <body className={`${inter.className} flex flex-col h-screen justify-between items-center w-screen`}>
         <AppWrapper>
         <ProgressBar/>
         <div className="flex-grow">{children}</div>
