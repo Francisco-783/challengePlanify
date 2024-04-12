b37dcd57c49ec2040c131a91fd989d199663e84e
Francisco.I <francisco.insaurralde21@gmail.com>
2024-04-12 06:39:43 -0300

    feature: when main page is render this separate all services in categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,60 @@ import 'tailwindcss/tailwind.css';
 import { useAppContext } from '@/app/context/index';
 import Link from 'next/link';
 import StepControls from './components/stepControls';
+import { useEffect } from 'react';
+import services from "./simulatedAPI/services.json"
 
 export default function Home() {
   const { storage, setStorage } = useAppContext(); 
-
-  const handleChangeName = () => {
-
-    const updatedStorage = {
-      ...storage, 
-      order: {
-        ...storage.order, 
-        name: 'Jose' 
-      }
-    };
-
-   
-    setStorage(updatedStorage);
+  type ServiceItem = {
+    id: number;
+    name: string;
+    description: string;
+    category: string;
   };
+  
+  type OrganizedService = {
+    category: string;
+    items: ServiceItem[];
+  };
+  
+  let organizedS: OrganizedService[] = [];
+
+  // const handleChangeName = () => {
+
+  //   const updatedStorage = {
+  //     ...storage, 
+  //     order: {
+  //       ...storage.order, 
+  //       name: 'Jose' 
+  //     }
+  //   };
+  //   setStorage(updatedStorage);
+  // };
+  const organizeService = () => {
+    services.services.forEach(service => {
+      const existingCategoryIndex = organizedS.findIndex(item => item.category === service.category);
+    
+      if (existingCategoryIndex !== -1) {
+        organizedS[existingCategoryIndex].items.push(service);
+      } else {
+        organizedS.push({ category: service.category, items: [service] });
+      }
+    });
+    console.log(organizedS)
+  }
+  useEffect(() => {
+    console.log("efect")
+    organizeService();
+  }, []);
   return (
     <main className='flex flex-col h-full justify-between'>
       <div>
-      <h1 >Challenge Planify {storage.order.name}</h1>
-      <button onClick={handleChangeName}>CAMBIO</button>
+      <h1 >Challenge Planify</h1>
+  
       <Link href={"/horarios"}>DSASDASD</Link>
       </div>
-      <StepControls linkBack="" linkNext="" completed={false} />
+      <StepControls linkNext="/horarios" completed={true} />
     </main>
   );
 }
