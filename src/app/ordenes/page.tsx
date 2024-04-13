3ab17884bee536ca0626719d0c0a51b19ee83cf4
Francisco.I <francisco.insaurralde21@gmail.com>
2024-04-13 14:16:38 -0300

    feature: page "ordenes" completed

--- src/app/ordenes/page.tsx
+++ src/app/ordenes/page.tsx
@@ -2,6 +2,8 @@
 import 'tailwindcss/tailwind.css';
 import { useEffect } from 'react';
 import { useAppContext } from '../context';
+import Link from 'next/link';
+import Button from '../components/button';
 
 export default function Ordenes() {
   const { storage, setStorage } = useAppContext();
@@ -9,20 +11,33 @@ export default function Ordenes() {
   useEffect(() => {
     setStorage(prevState => ({
       ...prevState,
+      progress: 4,
       reserving: false
     }));
-    return () => {
-      setStorage(prevState => ({
-        ...prevState,
-        reserving: true
-      }));
-    };
+
   }, []);
 
   return (
-    <main>
-      <h1>ordenes</h1>
-      <h1>{storage.order.name}</h1>
-    </main>
+
+    <div className='border-2 border-grey w-full mt-1 p-4'>
+      {storage.orderList.length === 0 ? (
+        <>
+          <h1 className='pb-1'>Ninguna orden registrada</h1>
+          <p className='bg-green-300 rounded p-1 mb-2.5'>Puedes hacer tu primera orden en la seccion "Reservar"</p>
+          <Link href={"/"}><Button text='Reservar'/></Link>
+        </>
+      ) : (
+        <>
+            {storage.orderList.map(reserva => (
+              <div className='w-64 border-2 border-grey mt-1 p-4'>
+                <h1>{reserva.name}</h1>
+                <h3>Servicio: {reserva.name}</h3>
+                <p>Fecha: {reserva.date} {reserva.time}</p>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+
   );
 }
