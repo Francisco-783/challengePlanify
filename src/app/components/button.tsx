2372159f5d04fd80b37e664431e52de531abde26
Francisco.I <francisco.insaurralde21@gmail.com>
2024-04-15 07:03:16 -0300

    feature: now the buttons are a little darker

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -12,7 +12,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ text, clickProcess, choosen, disable }) => {
   const [isChoosen, setIsChoosen] = useState(choosen || false);
 
-  const style = `hover:bg-gray-700 text-white transition-all font-bold py-1 px-4 rounded ${isChoosen ? 'bg-gray-600' : 'bg-gray-400'}`
+  const style = `hover:bg-gray-700 text-white transition-all font-bold py-1 px-4 rounded ${isChoosen ? 'bg-gray-700' : 'bg-gray-500'}`
   const disStyle = `text-white transition-all font-bold py-1 px-4 rounded bg-gray-300 cursor-not-allowed`
 
   useEffect(() => {
